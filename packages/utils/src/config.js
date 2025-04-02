9cd558b4dcd805c9b3fe78148b885219cd86a306
Pranav Mehta <94271480+DaPhysikist@users.noreply.github.com>
2025-04-02 14:31:11 -0700

    Update config.js

--- packages/utils/src/config.js
+++ packages/utils/src/config.js
@@ -1,2 +1,3 @@
-export const REGION = "REGION";
-export const IDENTITY_POOL_ID = "IDENTITY_POOL_ID";
+export const REGION = "us-west-1";
+export const IDENTITY_POOL_ID =
+  "us-west-1:e1298549-f1cc-4666-83a8-4b6e2839e310";
